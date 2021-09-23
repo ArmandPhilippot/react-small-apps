1b1d285d64289dd01823885390eeed16e12622a1
Armand Philippot <git@armandphilippot.com>
2021-09-23 12:14:48 +0200

    chore(meme): make text editable on click

--- meme-generator/src/components/Headline/Headline.js
+++ meme-generator/src/components/Headline/Headline.js
@@ -1,13 +1,45 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 function Headline({ position }) {
+  const inputEl = useRef(null);
   const [headline, setHeadline] = useState("Edit here");
+  const [isClicked, setIsClicked] = useState(false);
+  useEffect(() => {
+    isClicked && inputEl.current.focus();
+  });
 
-  return (
+  const onTextClick = () => {
+    setIsClicked(true);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsClicked(false);
+  };
+
+  const handleChange = (e) => {
+    setHeadline(e.target.value);
+  };
+
+  return isClicked ? (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className={`meme-generator__input meme-generator__headline meme-generator__headline--${
+          position === "first" ? "first" : "last"
+        }`}
+        value={headline}
+        ref={inputEl}
+        onChange={handleChange}
+        onBlur={() => setIsClicked(false)}
+      />
+    </form>
+  ) : (
     <p
       className={`meme-generator__headline meme-generator__headline--${
         position === "first" ? "first" : "last"
       }`}
+      onClick={onTextClick}
     >
       {headline}
     </p>
