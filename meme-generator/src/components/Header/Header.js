b39cb384b07432cb52c20740573482c72fa62642
Armand Philippot <git@armandphilippot.com>
2021-09-26 17:15:40 +0200

    refactor(meme): remove all sources to start again
    The current app is not working as expected. So I prefer to start
    again with a fresh install instead of trying to fix it.

--- meme-generator/src/components/Header/Header.js
+++
@@ -1,9 +0,0 @@
-function Header() {
-  return (
-    <header className="header">
-      <h1 className="header__branding">Meme Generator</h1>
-    </header>
-  );
-}
-
-export default Header;
--- meme-generator/src/components/commons/Option.js
+++
@@ -1,5 +0,0 @@
-function Option({ value = "Option", name = "option" }) {
-  return <option name={name}>{value}</option>;
-}
-
-export default Option;
