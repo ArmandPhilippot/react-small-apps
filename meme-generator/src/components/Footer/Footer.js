3587c6161b3e3e63b6a5d5b5700fe621e8bb54cd
Armand Philippot <git@armandphilippot.com>
2021-09-26 17:20:44 +0200

    chore(meme): add templates for header, footer and main

---
+++ meme-generator/src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <footer className="footer">
+      <p className="footer__copyright">
+        Meme Generator. MIT 2021. Armand Philippot.
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ meme-generator/src/components/Main/Main.js
@@ -0,0 +1,5 @@
+function Main() {
+  return <main className="main"></main>;
+}
+
+export default Main;
