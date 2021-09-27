c0c5b9188ebfab386c678430b50e4411029d84cc
Armand Philippot <git@armandphilippot.com>
2021-09-27 17:12:00 +0200

    chore(notebook): add react router & modern-normalize
    I also removed the defaults set by react create app.

--- notebook/src/index.js
+++ notebook/src/index.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { render } from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import reportWebVitals from "./reportWebVitals";
+import App from "./App";
+import "modern-normalize";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+render(
+  <BrowserRouter>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </BrowserRouter>,
+  document.getElementById("app")
 );
 
 // If you want to start measuring performance in your app, pass a function
