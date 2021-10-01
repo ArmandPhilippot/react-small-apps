56c475f887ad9bca00b9cc9d740d372b25f1d3d5
Armand Philippot <git@armandphilippot.com>
2021-10-01 21:59:05 +0200

    chore(notebook): fix textarea resize on input
    Some action was not capture like ctrl+z, cut, paste... So I modify the
    way I handle the textarea autoresize.

--- notebook/src/components/commons/FormElements/TextArea/TextArea.js
+++ notebook/src/components/commons/FormElements/TextArea/TextArea.js
@@ -1,11 +1,20 @@
 import { forwardRef, useEffect, useState } from "react";
 
-function autoGrow(field) {
-  let fieldHeight = field.style.height;
+function autoGrow(field, initialValue = null) {
+  let fieldHeight = initialValue ?? field.style.height;
   if (field.scrollHeight > field.clientHeight) {
     fieldHeight = field.scrollHeight + "px";
   }
   return fieldHeight;
+}
+
+function isSetHeightNeeded(e) {
+  const key = e.key;
+  const isBackspace = key === "Backspace";
+  const isDelete = key === "Delete";
+  const isCtrlZ = e.ctrlKey && e.key === "z";
+  const isCut = e.ctrlKey && e.key === "x";
+  return isBackspace || isDelete || isCtrlZ || isCut;
 }
 
 function TextArea(
@@ -18,7 +27,7 @@ function TextArea(
     : "form__textarea";
 
   useEffect(() => {
-    ref && setFieldHeight(() => autoGrow(ref.current));
+    ref && setFieldHeight(autoGrow(ref.current));
   }, [ref]);
 
   return (
@@ -27,9 +36,14 @@ function TextArea(
       className={classNames}
       name={name}
       value={value}
-      onChange={onChangeHandler}
+      onChange={(e) => {
+        onChangeHandler(e);
+        setFieldHeight(autoGrow(e.target));
+      }}
+      onKeyDown={(e) => {
+        if (isSetHeightNeeded(e)) setFieldHeight(autoGrow(e.target, "auto"));
+      }}
       onBlur={onBlurHandler}
-      onKeyUp={(e) => setFieldHeight(autoGrow(e.target))}
       style={{ height: fieldHeight }}
     />
   );
